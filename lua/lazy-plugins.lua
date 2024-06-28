bf1e691a25da77c2014b0e03ecdd84144779029a
Alex Efros <powerman@powerman.name>
2024-06-29 00:24:34 +0300

    refactor(plugins): move all loaded to plugins/

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,4 +1,4 @@
--- [[ Configure and install plugins ]]
+-- [[ Configure Lazy ]]
 --
 --  To check the current status of your plugins, run:
 --    :Lazy
@@ -7,48 +7,9 @@
 --
 --  To update plugins you can run:
 --    :Lazy update
---
---  Use `opts = {}` to force a plugin to be loaded. This is equivalent to:
---    require('Comment').setup({})
 require('lazy').setup {
   spec = {
-    'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically.
-
-    -- "gc" to comment visual regions/lines.
-    { 'numToStr/Comment.nvim', opts = {} },
-
-    require 'kickstart/plugins/gitsigns',
-
-    require 'kickstart/plugins/which-key',
-
-    require 'kickstart/plugins/telescope',
-
-    require 'kickstart/plugins/lspconfig',
-
-    require 'kickstart/plugins/conform',
-
-    require 'kickstart/plugins/cmp',
-
-    require 'kickstart/plugins/tokyonight',
-
-    require 'kickstart/plugins/todo-comments',
-
-    require 'kickstart/plugins/mini',
-
-    require 'kickstart/plugins/treesitter',
-
-    -- require 'kickstart.plugins.debug',
-    -- require 'kickstart.plugins.indent_line',
-    -- require 'kickstart.plugins.lint',
-    -- require 'kickstart.plugins.autopairs',
-    -- require 'kickstart.plugins.neo-tree',
-
-    -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-    --    This is the easiest way to modularize your config.
-    --
-    --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-    --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-    -- { import = 'custom.plugins' },
+    { import = 'plugins' },
   },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
---
+++ lua/plugins/init.lua
@@ -0,0 +1,10 @@
+-- [[ Install plugins without complicated configuration ]]
+--
+--  Use `opts = {}` to force a plugin to be loaded. This is equivalent to:
+--    require('Comment').setup({})
+return {
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically.
+
+  -- "gc" to comment visual regions/lines.
+  { 'numToStr/Comment.nvim', opts = {} },
+}
