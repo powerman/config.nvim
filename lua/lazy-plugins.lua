38c77193bb2a0801308bc51b5336ccb2dd6e4da2
Alex Efros <powerman@powerman.name>
2024-06-28 23:48:57 +0300

    feat(lazy-plugins): tune Lazy ui config

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -53,13 +53,14 @@ require('lazy').setup {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    border = 'rounded',
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
-      config = '🛠',
+      config = '⚙️ ',
       event = '📅',
       ft = '📂',
       init = '⚙',
-      keys = '🗝',
+      keys = '⌨️ ',
       plugin = '🔌',
       runtime = '💻',
       require = '🌙',
