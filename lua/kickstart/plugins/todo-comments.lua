8bc7133acaccdefc1e70a111000eb418e467de9e
Alex Efros <powerman@powerman.name>
2024-06-28 13:41:37 +0300

    fix(todo-comments): doublewidth icons

--- lua/kickstart/plugins/todo-comments.lua
+++ lua/kickstart/plugins/todo-comments.lua
@@ -1,5 +1,25 @@
 -- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    opts = {
+      signs = false,
+      -- Remove space after doublewidth icons to fix "Vim:E239: Invalid sign text".
+      -- Replace TEST icon because original one is missing from my fonts.
+      keywords = {
+        FIX = { icon = '' },
+        TODO = { icon = '' },
+        HACK = { icon = '' },
+        WARN = { icon = '' },
+        PERF = { icon = '' },
+        NOTE = { icon = '' },
+        TEST = { icon = '󰝖' },
+      },
+    },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
