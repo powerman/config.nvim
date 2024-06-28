468d62c20dd75b78936b6686d9a287de5afa69d2
Alex Efros <powerman@powerman.name>
2024-06-28 19:16:05 +0300

    chore(options): cleanup

--- lua/options.lua
+++ lua/options.lua
@@ -1,18 +1,14 @@
 -- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+-- See `:help vim.opt`.
+-- For more options, you can see `:help option-list`.
 
--- Make line numbers default
+-- Make line numbers default.
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
--- Don't show the mode, since it's already in the status line
+-- Don't show the mode, since it's already in the status line.
 vim.opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
