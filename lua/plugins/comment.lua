7972e314ce15dd490ecd0a021fe06ebe5a1e9468
Alex Efros <powerman@powerman.name>
2024-06-29 11:55:36 +0300

    feat(comment): map # to toggle line(s)

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,18 @@
+return {
+  { -- "gc", "#", "<C-C>" to comment visual regions/lines.
+    'numToStr/Comment.nvim',
+    opts = {},
+    config = function()
+      -- Add alternative mapping '#'.
+      vim.keymap.set('n', '#', function()
+        return vim.v.count == 0 and '<Plug>(comment_toggle_linewise_current)<Down>' or '<Plug>(comment_toggle_linewise_count)'
+      end, { expr = true, desc = 'Comment toggle linewise' })
+
+      vim.keymap.set('v', '#', '<Plug>(comment_toggle_linewise_visual)`><Down>', { desc = 'Comment toggle linewise' })
+
+      -- TODO: Add mappings for <C-C>.
+    end,
+  },
+}
+
+-- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,9 +4,6 @@
 --    require('Comment').setup({})
 return {
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically.
-
-  -- "gc" to comment visual regions/lines.
-  { 'numToStr/Comment.nvim', opts = {} },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
