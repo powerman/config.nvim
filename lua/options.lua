4040410793bef874f2ebe577369d34f5d509e00f
Alex Efros <powerman@powerman.name>
2024-06-29 12:07:07 +0300

    chore: replace modelines with editorconfig

--- lua/options.lua
+++ lua/options.lua
@@ -54,5 +54,3 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 6
-
--- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,5 +5,3 @@
 return {
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically.
 }
-
--- vim: ts=2 sts=2 sw=2 et
