089b0126d6c7b1d981f67088c64ae66e4410192d
Alex Efros <powerman@powerman.name>
2024-07-01 00:19:28 +0300

    feat(conform): enable ast-grep

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -25,7 +25,7 @@ return {
                 }
             end,
             formatters_by_ft = {
-                lua = { 'stylua' },
+                lua = { 'ast-grep', 'stylua' },
                 -- Conform can also run multiple formatters sequentially
                 -- python = { "isort", "black" },
                 --
