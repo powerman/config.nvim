25d23c20485677d8171c114bf4f7961614b6c342
Alex Efros <powerman@powerman.name>
2024-07-01 17:52:51 +0300

    feat(auto-session): add plugin

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,31 @@
+vim.opt.sessionoptions:append 'winpos'
+vim.opt.sessionoptions:append 'localoptions'
+
+---@type LazySpec
+return {
+    -- When starting nvim with no arguments, auto-session will try to restore an existing
+    -- session for the current cwd if one exists.
+    {
+        'rmagatti/auto-session',
+        dependencies = {
+            'nvim-telescope/telescope.nvim',
+        },
+        lazy = false,
+        keys = {
+            {
+                '<Leader>sa',
+                function()
+                    require('auto-session.session-lens').search_session()
+                end,
+                desc = '[S]earch [A]uto-sessions',
+            },
+        },
+        opts = {
+            auto_session_suppress_dirs = { '/', '~/', '~/proj/' },
+            session_lens = {
+                load_on_setup = true,
+                theme_conf = { border = true },
+            },
+        },
+    },
+}
