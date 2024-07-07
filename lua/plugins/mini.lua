feb336db4d5d8b6623ca05b5af923b522bca254e
Alex Efros <powerman@powerman.name>
2024-07-07 18:46:21 +0300

    feat(mini): setup

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,6 +2,7 @@
 return {
     { -- Collection of various small independent plugins/modules
         'echasnovski/mini.nvim',
+        version = '*',
         config = function()
             -- Better Around/Inside textobjects
             --
@@ -9,28 +10,54 @@ return {
             --  - va)  - [V]isually select [A]round [)]paren
             --  - yinq - [Y]ank [I]nside [N]ext [']quote
             --  - ci'  - [C]hange [I]nside [']quote
-            require('mini.ai').setup { n_lines = 500 }
+            local ai_gen_spec = require('mini.ai').gen_spec
+            require('mini.ai').setup {
+                n_lines = 500,
+                custom_textobjects = {
+                    F = ai_gen_spec.treesitter {
+                        a = '@function.outer',
+                        i = '@function.inner',
+                    },
+                    c = ai_gen_spec.treesitter {
+                        a = { '@conditional.outer', '@loop.outer' },
+                        i = { '@conditional.inner', '@loop.inner' },
+                    },
+                },
+            }
 
             -- Add/delete/replace surroundings (brackets, quotes, etc.)
             --
             -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
             -- - sd'   - [S]urround [D]elete [']quotes
             -- - sr)'  - [S]urround [R]eplace [)] [']
-            require('mini.surround').setup()
+            local sur_ts_input = require('mini.surround').gen_spec.input.treesitter
+            require('mini.surround').setup {
+                custom_surroundings = {
+                    -- Use tree-sitter to search for function call
+                    f = {
+                        input = sur_ts_input { outer = '@call.outer', inner = '@call.inner' },
+                    },
+                },
+            }
 
             -- Simple and easy statusline.
             --  You could remove this setup call if you don't like it,
             --  and try some other statusline plugin
-            local statusline = require 'mini.statusline'
-            -- set use_icons to true if you have a Nerd Font
-            statusline.setup { use_icons = vim.g.have_nerd_font }
+            require('mini.statusline').setup { use_icons = vim.g.have_nerd_font }
 
             -- You can configure sections in the statusline by overriding their
             -- default behavior. For example, here we set the section for
             -- cursor location to LINE:COLUMN
             ---@diagnostic disable-next-line: duplicate-set-field
-            statusline.section_location = function()
-                return '%2l:%-2v'
+            MiniStatusline.section_location = function()
+                local slash = MiniStatusline.config.use_icons and '' or ' /'
+                return '%3l:%-2v' .. slash .. '%L'
+            end
+            local section_filename = MiniStatusline.section_filename
+            ---@diagnostic disable-next-line: duplicate-set-field
+            MiniStatusline.section_filename = function(args)
+                -- Force short filename.
+                return string.gsub(section_filename(args), '%%F', '%%f')
             end
 
             -- ... and there is more!
