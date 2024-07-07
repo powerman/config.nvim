8065f42a64eb114da696335b628fbb601c8a7f78
Alex Efros <powerman@powerman.name>
2024-07-07 18:50:23 +0300

    feat(treesitter): add nvim-treesitter-textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -42,4 +42,8 @@ return {
             --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
         end,
     },
+    {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    },
 }
