a21acf1b30639a99a1d71d1f3d05a0e28a822cb6
Alex Efros <powerman@powerman.name>
2024-07-09 01:13:57 +0300

    feat(auto-session): automate overwriting sessions

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -2,19 +2,19 @@
 --
 --  1. When starting `nvim` with no arguments, auto-session will try to restore an existing
 --     session for the `cwd` (current working directory) if one exists.
---  2. When starting `nvim` with some argument, auto-session will do nothing.
---  3. Even after starting `nvim` with an argument, a session can still be manually restored
---     by running `:SessionRestore`.
---  4. Any session saving and restoration takes into consideration the `cwd`.
---  5. When piping to nvim, e.g: `cat myfile | nvim`, auto-session behaves like #2.
+--  2. When starting `nvim dir`, auto-session will try to restore the
+--     session for that directory.
+--  3. When starting `nvim file …` auto-session won't try to restore an existing session and
+--     will save (overwrite existing) session only if `args_allow_files_auto_save` option
+--     is|returns true.
+--  4. Even after starting nvim with a file argument, a session can still be manually restored
+--     by running :SessionRestore or manually saved by running :SessionSave.
+--  5. Any session saving and restoration takes into consideration the `cwd`.
+--  6. When piping to nvim, e.g: `cat myfile | nvim`, auto-session won't do anything.
 --
 --  See https://github.com/rmagatti/auto-session.
 --
--- NOTE: Cmd `:SessionSave` manually saves a session.
--- NOTE: Key `<Leader>sa` opens session manager (to view/open/delete sessions).
---
--- TODO: Automate `:SessionSave` if more than one tab/window open at VimExit.
--- https://github.com/rmagatti/auto-session/issues/316
+-- NOTE: Key `<Leader>sa` opens session manager (to view/restore/delete sessions).
 
 ---@type LazySpec
 return {
@@ -47,6 +47,21 @@ return {
                 load_on_setup = true,
                 theme_conf = { border = true },
             },
+            -- If `nvim` was started with file(s) arg(s) and previous session wasn't restored,
+            -- then save the session if exiting with at least two tabs/windows with usual files.
+            args_allow_files_auto_save = function()
+                local valuable = 0
+                for _, tabpage in ipairs(vim.api.nvim_list_tabpages()) do
+                    for _, window in ipairs(vim.api.nvim_tabpage_list_wins(tabpage)) do
+                        local buffer = vim.api.nvim_win_get_buf(window)
+                        local file_name = vim.api.nvim_buf_get_name(buffer)
+                        if file_name ~= '' and vim.fn.filereadable(file_name) then
+                            valuable = valuable + 1
+                        end
+                    end
+                end
+                return valuable > 1
+            end,
         },
     },
 }
