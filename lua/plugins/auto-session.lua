e7f897087835abf3df311e46f44d6a1ea280dba1
Alex Efros <powerman@powerman.name>
2024-07-08 22:16:00 +0300

    docs(auto-session): add

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,17 +1,30 @@
-vim.opt.sessionoptions:append 'winpos'
-vim.opt.sessionoptions:append 'localoptions'
+-- [[ A small automated session manager for Neovim ]]
+--
+--  1. When starting `nvim` with no arguments, auto-session will try to restore an existing
+--     session for the `cwd` (current working directory) if one exists.
+--  2. When starting `nvim` with some argument, auto-session will do nothing.
+--  3. Even after starting `nvim` with an argument, a session can still be manually restored
+--     by running `:SessionRestore`.
+--  4. Any session saving and restoration takes into consideration the `cwd`.
+--  5. When piping to nvim, e.g: `cat myfile | nvim`, auto-session behaves like #2.
+--
+--  See https://github.com/rmagatti/auto-session.
+--
+-- NOTE: Cmd `:SessionSave` manually saves a session.
+-- NOTE: Key `<Leader>sa` opens session manager (to view/open/delete sessions).
+--
+-- TODO: Automate `:SessionSave` if more than one tab/window open at VimExit.
+-- https://github.com/rmagatti/auto-session/issues/316
 
 ---@type LazySpec
 return {
-    -- When starting nvim with no arguments, auto-session will try to restore an existing
-    -- session for the current cwd if one exists.
     {
         'rmagatti/auto-session',
         version = '*',
         dependencies = {
             'nvim-telescope/telescope.nvim',
         },
-        lazy = false,
+        lazy = false, -- Needs to restore session on Neovim start.
         keys = {
             {
                 '<Leader>sa',
@@ -21,6 +34,10 @@ return {
                 desc = '[S]earch [A]uto-sessions',
             },
         },
+        init = function()
+            vim.opt.sessionoptions:append 'winpos'
+            vim.opt.sessionoptions:append 'localoptions'
+        end,
         opts = {
             auto_session_suppress_dirs = { '/', '~/', '~/proj/' },
             session_lens = {
