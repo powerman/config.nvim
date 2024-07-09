a5c3c57491ab190d00e09a69869c1025298492a1
Alex Efros <powerman@powerman.name>
2024-07-10 01:56:59 +0300

    refactor(ftplugin/help): avoid vim.api

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -1,7 +1,7 @@
 vim.opt_local.signcolumn = 'auto'
 
-vim.api.nvim_buf_set_keymap(0, 'n', 'q', ':lclose<CR><C-W>q', {
-    noremap = true,
+vim.keymap.set('n', 'q', '<Cmd>lclose<CR><Cmd>quit<CR>', {
+    buffer = true,
     nowait = true,
     silent = true,
     desc = '[Q]uit the current window',
