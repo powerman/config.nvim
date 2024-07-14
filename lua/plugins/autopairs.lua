eb9e337368a890bf5a3e3522049a11c62717abf5
Alex Efros <powerman@powerman.name>
2024-07-14 22:24:42 +0300

    feat(autopairs): add

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,13 @@
+-- [[ A super powerful autopair plugin for Neovim that supports multiple characters ]]
+--
+--  - Automatically adds pair for a quote/bracket/paren/etc.
+--  - Automatically indent when pressing <CR> inside bracket/paren/etc.
+
+---@type LazySpec
+return {
+    'windwp/nvim-autopairs',
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    lazy = true,
+    event = 'InsertEnter',
+    config = true,
+}
