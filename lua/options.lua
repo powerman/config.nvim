5548082cfa874667dd607fe9276b0a1c700927bf
Alex Efros <powerman@powerman.name>
2024-07-17 15:50:31 +0300

    chore(options): cleanup

--- lua/options.lua
+++ lua/options.lua
@@ -10,44 +10,40 @@ vim.opt.mouse = 'a'
 vim.opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
 vim.opt.clipboard = 'unnamedplus'
 
--- Enable break indent
+-- Enable break indent.
 vim.opt.breakindent = true
 
--- Save undo history
+-- Save undo history.
 vim.opt.undofile = true
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term.
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
--- Keep signcolumn on by default
+-- Keep signcolumn on by default.
 vim.opt.signcolumn = 'yes'
 
--- Decrease update time
+-- Decrease update time.
 vim.opt.updatetime = 250
 
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
+-- Decrease mapped sequence wait time.
+-- Displays which-key popup sooner. TODO: Not used in which-key v3.
 vim.opt.timeoutlen = 300
 
--- Configure how new splits should be opened
+-- Configure how new splits should be opened.
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
 
--- Show which line your cursor is on
+-- Show which line your cursor is on.
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
@@ -59,5 +55,5 @@ vim.opt.keymodel = 'startsel'
 -- Disable wrapping.
 vim.opt.wrap = false
 
--- When formatting text (e.g. using `gq` or `gw`), recognize numbered lists
+-- When formatting text (e.g. using `gq` or `gw`), recognize numbered lists.
 vim.opt.formatoptions:append 'n'
