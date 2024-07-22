26b6496375f3f4a4af24f7f46702870682cd6cb4
Alex Efros <powerman@powerman.name>
2024-07-22 22:19:36 +0300

    docs(tests): add useful links

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,3 +1,8 @@
+-- Documentation:
+--  - https://github.com/nvim-lua/plenary.nvim/blob/master/TESTS_README.md
+--  - https://github.com/lunarmodules/luassert (plenary vendors some version of it)
+--  - https://lunarmodules.github.io/busted/ (plenary mimics part of it)
+
 vim.opt.rtp:append '~/.local/share/nvim/lazy/plenary.nvim'
 
 require 'patch.plenary.busted_format_results'
