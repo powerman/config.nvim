564a9a119899108b60f7f13b3dbe3adc5bd56970
Alex Efros <powerman@powerman.name>
2025-05-17 10:48:30 +0300

    chore(init): cleanup

--- init.lua
+++ init.lua
@@ -1,24 +1,25 @@
 --[[ Configuration ]]
 --
+--  INFO: Use <Leader>sc to search cheatsheet with most useful features of this configuration.
+--
 --  See https://learnxinyminutes.com/docs/lua/.
 --  See `:help lua-guide`.
 
--- Run `nvim --cmd 'let debug_lsp=1'` to enable LSP debugging.
-vim.g.debug_lsp = vim.g.debug_lsp or false
+-- NOTE:  nvim --cmd 'let debug_lsp=1'   LSP: Enable debugging.
 
--- Set <space> as the leader key.
+-- Use <Space> as both <Leader> and <LocalLeader> keys.
 -- See `:help mapleader`.
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+-- Set to false if you prefer to manually open diagnostic with <Leader>e.
+vim.g.auto_open_diagnostic = true
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal.
 vim.g.have_nerd_font = true
 
 -- Set to true if you use a Nerd Font with "Mono" suffix in font name (small icons).
 vim.g.mono_nerd_font = false
-
--- Set to false if you prefer to manually open diagnostic with <Leader>e.
-vim.g.auto_open_diagnostic = true
 
 -- Border for floating windows.
 -- See allowed values in `:h nvim_open_win()`.
@@ -33,6 +34,9 @@ vim.g.float_max_width = 0.96
 -- Auto-detected for urxvt terminal, false in other terminals.
 vim.g.transparent = string.find(vim.env.TERM, '^rxvt') ~= nil
     and string.find(vim.env.COLORFGBG, '%d$') == nil
+
+-- You can start Neovim this way to enable it: `nvim --cmd 'let debug_lsp=1'`.
+vim.g.debug_lsp = vim.g.debug_lsp or false
 
 -- Setup Nerd Fonts.
 require 'nerd-fonts'
