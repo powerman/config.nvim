6bd02483e6342bae17324872bcbf80f0a1cae741
Alex Efros <powerman@powerman.name>
2025-06-16 15:22:51 +0300

    feat(autocmd): manual folding markdown sections

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -41,3 +41,51 @@ vim.api.nvim_create_autocmd('BufWinEnter', {
         end
     end,
 })
+
+-- Enable manual convenient folding for markdown sections.
+vim.api.nvim_create_autocmd('BufWinEnter', {
+    desc = 'Setup folding per filetype',
+    group = vim.api.nvim_create_augroup('user.folding', { clear = true }),
+    callback = function(ev)
+        if vim.bo.filetype == 'markdown' then
+            vim.wo.foldmethod = 'expr'
+            vim.wo.foldexpr =
+                [[getline(v:lnum) =~# '^#' ? '>' . matchend(getline(v:lnum), '^#*') : '=']]
+            -- Remove fold-related info from a folded line (markdown sections already include level).
+            vim.wo.foldtext = [[getline(v:foldstart)]]
+        else
+            -- Workaround for `tab sball` (used in other autocmd above) which will result in
+            -- copying window-local options to new windows/tabs and thus apply them to other
+            -- files even if they have another filetypes. So, reset these options to defaults.
+            vim.wo.foldmethod = 'manual'
+            vim.wo.foldexpr = '0'
+            vim.wo.foldtext = [[foldtext()]]
+        end
+
+        -- Do not modify folds restored by loading session.
+        if vim.g.SessionLoad then
+            return
+        end
+        -- Auto-open all folds on first open.
+        -- Use defer_fn for compatibility with opening files with Telescope
+        -- (which opens files using async file loading and file contents may not be loaded
+        -- at the moment BufWinEnter - or other events like FileReadPost - fires).
+        local win = vim.api.nvim_get_current_win()
+        vim.defer_fn(function()
+            if
+                vim.api.nvim_win_is_valid(win)
+                and vim.api.nvim_win_get_buf(win) == ev.buf
+                and not vim.wo[win].diff
+            then
+                vim.api.nvim_win_call(win, function()
+                    if vim.bo[ev.buf].filetype == 'markdown' then
+                        vim.cmd 'normal! zR'
+                    else
+                        vim.cmd 'normal! zE'
+                        vim.cmd 'normal! zx'
+                    end
+                end)
+            end
+        end, 30) -- It's a race, of course. Increase in case of issues.
+    end,
+})
