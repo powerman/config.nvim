360d98a45dd7213818f5b2c02bea0a6e34f60995
Alex Efros <powerman@powerman.name>
2026-02-27 15:53:52 +0200

    fix(lazy): bootstrap use commit from lazy.lock

--- lua/lazy-bootstrap.lua
+++ lua/lazy-bootstrap.lua
@@ -5,16 +5,36 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
     local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    local out = vim.fn.system {
+    -- Try to read pinned commit from lazy-lock.json
+    local lock_commit
+    local lock_file = vim.fn.stdpath 'config' .. '/lazy-lock.json'
+    local f = io.open(lock_file, 'r')
+    if f then
+        local content = f:read '*a'
+        f:close()
+        local lock = vim.json.decode(content)
+        lock_commit = lock and lock['lazy.nvim'] and lock['lazy.nvim'].commit
+    end
+    local clone_args = {
         'git',
         'clone',
         '--filter=blob:none',
-        '--branch=stable',
-        lazyrepo,
-        lazypath,
     }
+    if lock_commit then
+        -- Clone without branch restriction to allow checkout of arbitrary commit
+        vim.list_extend(clone_args, { '--no-checkout', lazyrepo, lazypath })
+    else
+        vim.list_extend(clone_args, { '--branch=stable', lazyrepo, lazypath })
+    end
+    local out = vim.fn.system(clone_args)
     if vim.v.shell_error ~= 0 then
         error('Error cloning lazy.nvim:\n' .. out)
+    end
+    if lock_commit then
+        out = vim.fn.system { 'git', '-C', lazypath, 'checkout', lock_commit }
+        if vim.v.shell_error ~= 0 then
+            error('Error checking out lazy.nvim commit ' .. lock_commit .. ':\n' .. out)
+        end
     end
 end
 vim.opt.rtp:prepend(lazypath)
